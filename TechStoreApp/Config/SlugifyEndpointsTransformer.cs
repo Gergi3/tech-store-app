a748c4609700bb5f7cdf23fe43d99150e76df1e5
Georgi Petrov <hailanimepenchou@gmail.com>
2024-03-27 13:51:02 +0200

    Optimize slugging algorithm
    closes #15

--- TechStoreApp/Config/SlugifyEndpointsTransformer.cs
+++ TechStoreApp/Config/SlugifyEndpointsTransformer.cs
@@ -1,23 +1,40 @@
+using System.Globalization;
+using System.Text;
 using TechStoreApp.Common.Extensions;
 
 namespace TechStoreApp.Config;
 
 public class SlugifyEndpointsTransformer : IOutboundParameterTransformer
 {
-	public string TransformOutbound(object? value)
+	public string? TransformOutbound(object? str)
 	{
-		if (value == null)
+		if (str == null)
 		{
 			return null!;
 		}
 
-		string? valueStringified = value.ToString();
+		string? valueStringified = str.ToString();
 
 		if (string.IsNullOrEmpty(valueStringified))
 		{
 			return null!;
 		}
 
-		return valueStringified.ToSlug();
+		var sb = new StringBuilder();
+		for (var i = 0; i < valueStringified.Length; i++)
+		{
+			if (i != 0
+				&& char.IsUpper(valueStringified[i])
+				&& char.IsLower(valueStringified[i - 1]))
+			{
+				sb.Append(CultureInfo.InvariantCulture, $"-{valueStringified[i]}");
+			}
+			else
+			{
+				sb.Append(valueStringified[i]);
+			}
+		}
+
+		return sb.ToString().ToSlug();
 	}
 }
