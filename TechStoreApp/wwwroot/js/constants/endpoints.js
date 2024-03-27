aa2091b23380a6791bb6942109a7228e1b433fd2
Georgi Petrov <hailanimepenchou@gmail.com>
2024-03-27 13:33:06 +0200

    Replace double quotes to single quotes in .ts files

--- TechStoreApp/wwwroot/js/constants/endpoints.js
+++ TechStoreApp/wwwroot/js/constants/endpoints.js
@@ -1,8 +1,8 @@
 export const APP_HOST = 'https://localhost:7152';
-export const buildUrl = (endpoint) => APP_HOST + "/" + endpoint;
+export const buildUrl = (endpoint) => APP_HOST + '/' + endpoint;
 export const endpoints = {
     category: {
-        list: buildUrl("categories/category-list")
+        list: buildUrl('categories/category-list')
     }
 };
 //# sourceMappingURL=endpoints.js.map
--- TechStoreApp/wwwroot/js/constants/endpoints.ts
+++ TechStoreApp/wwwroot/js/constants/endpoints.ts
@@ -1,10 +1,10 @@
 export const APP_HOST = 'https://localhost:7152';
 
-export const buildUrl = (endpoint: string) => APP_HOST + "/" + endpoint;
+export const buildUrl = (endpoint: string) => APP_HOST + '/' + endpoint;
 
 export const endpoints = {
 	category: {
-		list: buildUrl("categories/category-list")
+		list: buildUrl('categories/category-list')
 	}
 };
 
